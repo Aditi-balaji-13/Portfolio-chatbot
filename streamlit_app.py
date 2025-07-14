6e3009e70bdc8eade775129418b12ff3182a1d93
Aditi Balaji <79737130+Aditi-balaji-13@users.noreply.github.com>
2025-07-14 00:28:15 +0000

    force push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,51 @@
 import streamlit as st
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_ollama import OllamaLLM
+from langchain_together import ChatTogether
+import os
 
-st.title("🎈Aditi Balaji - A chat")
-st.write(
-    "Hey! This is  chatbot you can ask questions about Aditi to. Her Master resume is linked here if you would like a peek. Here is a list of questiona you can ask" \
-    " if you are out of ideas! \n" \
-    "\nQuestions"
+# Short description of the app
+# Header
+st.title("✨ Ask Aditi's AI-Powered Profile Assistant")
 
-)
+# Body
+st.markdown("""
+Welcome to **Aditi's Chatbot**! 🤖  
+This is a personalized chatbot trained on Aditi Balaji’s resume, experiences, and creative chaos. This application was built 
+using Streamlit, Langchain, and TogetherAI. It allows you to ask questions about Aditi's education, projects, skills, and more.
+            
+(Disclaimer: This is a chatbot that is not finetuned completely and hence may generate nonesence at times.)
+            
+---
 
+### 💼 What can you ask?
+- Education & Projects: _“Where did Aditi study?”, “What’s her NASA project about?”_
+- Tech Stack & Skills: _“What tools does she use in machine learning?”_
+- Career: _“Where has Aditi interned?”_
+- Hobbies & Art: _“Has she written any funny song parodies?”_
+- Books & Blogs: _“What's her favorite book?”, “What’s ‘A Fiery Mind’?”_
+
+---
+
+### 🎭 Fun Questions to Try
+- _“What are some song parodies Aditi made?"_
+- _“Is Aditi an alien?”_
+- _“Where can I follow Aditi’s creative work?”_
+- _“What’s Aditi’s favorite book?”_
+
+---
+
+### 🔗 Github and Linkedin
+- https://github.com/Aditi-balaji-13
+- https://www.linkedin.com/in/aditi-balaji-13/
+
+Type your question below and see how Aditi's AI assistant responds!
+""")
+
+
+# Clear chat history button
 def clear_chat_history():
     st.session_state.messages = []
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
@@ -24,6 +62,32 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
+# RAG pip line 
+# Retriever
+embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+vectordb = FAISS.load_local("faiss_store", embeddings=embedding, allow_dangerous_deserialization=True)
+retriever = vectordb.as_retriever(search_kwargs={"k": 1})
+
+# Replicate to host ollama
+#replicate.Client(api_token=st.secrets["REPLICATE_API"])
+os.environ['TOGETHER_API_TOKEN'] = st.secrets["TOGETHER_API"]
+llm = ChatTogether(
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    temperature=0.1,
+    max_tokens=1000,
+    top_p=0.9,
+    api_key=st.secrets["TOGETHER_API"]
+)
+# LLM
+# llm = OllamaLLM(model="llama3.1", temperature=0.1, max_tokens=1000)
+
+# RAG Chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=False
+)
 
 # Accept user input
 if prompt := st.chat_input("What would you like to ask"):
@@ -32,7 +96,7 @@ if prompt := st.chat_input("What would you like to ask"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    response = f"The asnwer to question {prompt}"
+    response = qa_chain.run(prompt)
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
--- utils.py
+++ utils.py
@@ -5,35 +5,24 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import Chroma
 
-GITHUB_TOKEN = os.environ.get("ITHUB_TOKEN")  # Make sure this is set in your env
 
-REPO = "Aditi-balaji-13/chatbot_docs"
-PDF_FILES = ["Master_resume.pdf", "Other.pdf"]
+
+#GITHUB_TOKEN = os.environ.get("ITHUB_TOKEN")  # Make sure this is set in your env
+
+#REPO = "Aditi-balaji-13/chatbot_docs"
+PDF_FILES = ["Maaster_resume.pdf", "Other.pdf"]
 CHROMA_DIR = "chroma_store"
 
-headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+#headers = {"Authorization": f"token {GITHUB_TOKEN}"}
 
-def fetch_file_from_private_repo(repo, filepath, local_name=None):
-    url = f"https://raw.githubusercontent.com/{repo}/main/{filepath}"
-    if not local_name:
-        local_name = os.path.basename(filepath)
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        with open(local_name, "wb") as f:
-            f.write(response.content)
-        print(f"✅ Downloaded {local_name}")
-        return local_name
-    else:
-        print(f"❌ Failed to fetch {filepath}: {response.status_code}")
-        return None
 
 def build_chroma():
     all_docs = []
     for file in PDF_FILES:
-        local_file = fetch_file_from_private_repo(REPO, file)
-        if local_file:
-            loader = PyPDFLoader(local_file)
+        
+        if file:
+            loader = PyPDFLoader(file)
             docs = loader.load()
             splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
             chunks = splitter.split_documents(docs)
